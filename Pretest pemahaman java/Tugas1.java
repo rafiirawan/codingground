0363a5fac86e3dfba8920399524d5ed8fb7891db
rafiirawan <rafiirawan7@gmail.com>
2017-02-27 11:18:37 +0700

    Delete Tugas1.java

--- Pretest pemahaman java/Tugas1.java
+++
@@ -1,8 +0,0 @@
-public class Tugas1{
-
-     public static void main(String []args){
-        String nama = "Rafi Irawan";
-        String kota = "Jepara";
-        System.out.println("Nama : "+nama+" dari kota: "+kota);
-     }
-}
