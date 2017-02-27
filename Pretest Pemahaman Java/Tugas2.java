e30a5f999a3b50ca68b93902c7a2ebac64904efb
rafiirawan <rafiirawan7@gmail.com>
2017-02-27 11:49:57 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,10 @@
+public class Tugas2{
+
+     public static void main(String []args){
+        String[] charArray ={ "rafi", "ira", "wan" }; 
+        
+        for (int i = 0; i < charArray.length; i++) {
+         System.out.println(charArray[i] + " ");
+        }
+     }
+}
